9dec6b014ebf3a6ef95784335248f81a64c762af
VANHILLE Francois <francois.vanhille@ext.europarl.europa.eu>
2022-06-25 12:54:19 +0200

    Integrated Conjured Items
    Added Conjured Item
    Updated quality counts for Conjured in GildedRose algorithm
    Added unit tests

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -13,7 +13,13 @@ class GildedRose {
         for (Item item : items) {
             if (!item.name.equals(AGED_BRIE) && !item.name.equals(BACKSTAGE_PASSES)) {
                 if (item.quality > 0) {
-                    if (!item.name.equals(SULFURAS)) {
+                    if (item.name.equals(CONJURED)) {
+                        if (item.quality == 1) {
+                            item.quality = 0;
+                        } else {
+                            item.quality -= 2;
+                        }
+                    } else if (!item.name.equals(SULFURAS)) {
                         item.quality = item.quality - 1;
                     }
                 }
@@ -39,26 +45,29 @@ class GildedRose {
 
             if (!item.name.equals(SULFURAS)) {
                 item.sellIn = item.sellIn - 1;
-            }
 
-            if (item.sellIn < 0) {
-                if (item.name.equals(AGED_BRIE)) {
+                if (item.sellIn < 0) {
+                    if (item.name.equals(AGED_BRIE)) {
 //                    if (item.quality < 50) {
 //                        // FIXME Not mentioned that quality of Brie increases twice as much once the sell by date has passed
 //                        item.quality = item.quality + 1;
 //                    }
-                } else {
-                    if (item.name.equals(BACKSTAGE_PASSES)) {
+                    } else if (item.name.equals(BACKSTAGE_PASSES)) {
                         item.quality = 0;
+                    } else if (item.name.equals(CONJURED)) {
+                        if (item.quality >= 2) {
+                            item.quality -= 2;
+                        } else {
+                            item.quality = 0;
+                        }
                     } else {
                         if (item.quality > 0) {
-                            if (!item.name.equals(SULFURAS)) {
-                                item.quality = item.quality - 1;
-                            }
+                            item.quality = item.quality - 1;
                         }
                     }
                 }
             }
+
         }
     }
 
--- Java/src/main/java/com/gildedrose/helpers/ItemUtils.java
+++ Java/src/main/java/com/gildedrose/helpers/ItemUtils.java
@@ -3,9 +3,10 @@ package com.gildedrose.helpers;
 import com.gildedrose.Item;
 
 public class ItemUtils {
+    public static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
     public static final String AGED_BRIE = "Aged Brie";
     public static final String BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert";
-    public static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
+    public static final String CONJURED = "Conjured";
 
     public static boolean isNameEquals(Item item, String name) {
         return item.name.equals(name);
