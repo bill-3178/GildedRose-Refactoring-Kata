1715d18761ccb6af4cd49840ec437a95750a801e
VANHILLE Francois <francois.vanhille@ext.europarl.europa.eu>
2022-06-27 20:02:23 +0200

    'backstage' quality increase algo updated
    limit numbers of days for 'backstage' quality increase are included
    'Sulfuras' tests refactoring
    Added unit tests

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -11,39 +11,36 @@ class GildedRose {
 
     public void updateQuality() {
         for (Item item : items) {
-            if (!item.name.equals(AGED_BRIE) && !item.name.equals(BACKSTAGE_PASSES)) {
-                if (item.quality > 0) {
-                    if (item.name.equals(CONJURED)) {
-                        if (item.quality == 1) {
-                            item.quality = 0;
-                        } else {
-                            item.quality -= 2;
-                        }
-                    } else if (!item.name.equals(SULFURAS)) {
-                        item.quality = item.quality - 1;
-                    }
-                }
+            if (item.name.equals(SULFURAS)) {
+                item.quality = SULFURAS_QUALITY; // FIXME or set to MAX at initialization and then never changed ?
             } else {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
-
-                    if (item.name.equals(BACKSTAGE_PASSES)) {
-                        if (item.sellIn < 11) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
+                if (!item.name.equals(AGED_BRIE) && !item.name.equals(BACKSTAGE_PASSES)) {
+                    if (item.quality > 0) {
+                        if (item.name.equals(CONJURED)) {
+                            if (item.quality == 1) {
+                                item.quality = 0;
+                            } else {
+                                item.quality -= 2;
                             }
+                        } else {
+                            item.quality = item.quality - 1;
                         }
+                    }
+                } else {
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1;
 
-                        if (item.sellIn < 6) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
+                        if (item.name.equals(BACKSTAGE_PASSES)) {
+                            // limits have been included
+                            if (item.sellIn <= 6) {
+                                item.quality += 2;
+                            } else if (item.sellIn <= 11) {
+                                item.quality++;
                             }
                         }
                     }
                 }
-            }
 
-            if (!item.name.equals(SULFURAS)) {
                 item.sellIn = item.sellIn - 1;
 
                 if (item.sellIn < 0) {
@@ -67,7 +64,6 @@ class GildedRose {
                     }
                 }
             }
-
         }
     }
 
